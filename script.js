59c31f26832af063c167de4c43d216113e7dc8c9
Rajnandanibais <rajnandanisinghbais@gmail.com>
2021-05-24 20:06:34 +0530

    script complete

--- script.js
+++ script.js
@@ -33,23 +33,41 @@ const validEmail = function(input){
 
 //check email
 const checkEmail =function(input){
-
-
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(re.test(String(input.value).toLowerCase())){
      showSuccess(input);
     }else{
-        showErorr(email,'email is invalid');
+        showErorr(input,'email is invalid');
     }
-
-
-
-
+};
+//min max funv
+const checkLength =function(input,min,max){
+    if(input.value.length<min){
+        showErorr(input,`${message(input)}must be at least ${min} character`);
+    }else if(input.value.length>max){
+        showErorr(input,`${message}must be less than ${max} character`);
+    }else{
+        showSuccess(input);
 }
+};
+// check password func
+const checkPassword = function (input1,input2){
+    if (input1.value !== '' && input2.value !== '')
+      if (input1.value !== input2.value) {
+        showError(input2,'Password not matched');
+      } else {
+        showSuccess(input1);
+        showSuccess(input2);
+      }
+  };
 
 
-
-
+  //func for mess
+  const message = function (input) {
+    var errorMessage = input.id.replace(/-p/, ' P');
+    return errorMessage.charAt(0).toUpperCase() + errorMessage.slice(1);
+  };
+// func for refactor code
 const checkRequired = function(inputarr){
     inputarr.forEach((input) =>{
    if (input.value ===''){
@@ -64,7 +82,10 @@ const checkRequired = function(inputarr){
 
 form.addEventListener('submit',(e) =>{
 e.preventDefault();
-checkRequired(inputarr);
+ checkRequired(inputarr);
+ checkEmail(email);
+ checkLength(password,6,12);
+ checkPassword(password,confirmpassword);
 });
 
 
